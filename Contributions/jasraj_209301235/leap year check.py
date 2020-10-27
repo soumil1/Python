0177465a1e3e58108f5524b7ca7e7099356e5be6
Jasraj Singh Juneja <factualsquirrel@gmail.com>
2020-10-28 01:51:09 +0530

    user enters the year intead of it being pre defined

---
+++ Contributions/jasraj_209301235/leap year check.py
@@ -0,0 +1,14 @@
+#user enters the year instead of it being pre-defined
+
+year = int(input("Enter Year: "))
+
+
+if year % 4 == 0 and year % 100 != 0:
+    print(year, "is a Leap Year")
+elif year % 100 == 0:
+    print(year, "is not a Leap Year")
+elif year % 400 ==0:
+    print(year, "is a Leap Year")
+else:
+    print(year, "is not a Leap Year")
+
